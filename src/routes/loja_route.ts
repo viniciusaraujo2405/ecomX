2e0bfb28d2722ce73ce1cdfd4c17cc12295f6341
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-05-15 12:01:57 -0300

    Criando rota para loja

---
+++ src/routes/loja_route.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { LojaController } from '../controllers/loja_controller';
+
+const router = Router();
+const controller = new LojaController();
+
+router.post('/', controller.create);
+router.get('/', controller.findAll);
+router.get('/:id', async (req, res) => {
+	try {
+		await controller.findById(req, res);
+	} catch (error) {
+		res.status(500).send({ error: 'Internal Server Error' });
+	}
+});
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
+
+export default router;
